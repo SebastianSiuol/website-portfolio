f99932c3ebea322523a855d9a6fb4e6f055e3546
SebastianSiuol <sebastianlouismtorio@iskolarngbayan.pup.edu.ph>
2025-10-31 14:58:49 +0800

    feat: refactor Navbar component; improve styling and structure; add hamburger menu functionality for mobile navigation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,76 @@
+import { useState } from "react";
+
 import "./NavBar.css";
 
+import { GiHamburgerMenu } from "react-icons/gi";
+
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+    function menuBurger() {
+        setMenuOpen((i) => !i);
+    }
 
     return (
-        <nav className="navbar-container">
-            <div className="navbar">
-                <a href="#home" className="logo">
-                    Torio's Portfolio
-                </a>
-                <ul className="nav-links">
+        <>
+            <nav className="navbar">
+                <div className="navbar-content-links">
+                    <a href="#home" className="logo">
+                        Torio's Portfolio
+                    </a>
+                    <ul className="nav-links">
+                        <li>
+                            <a href="#home">Home</a>
+                        </li>
+                        <li>
+                            <a href="#about">About</a>
+                        </li>
+                        <li>
+                            <a href="#projects">Projects</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="navbar-content-menu">
+                    <a href="#home" className="logo">
+                        Torio's Portfolio
+                    </a>
+                    <div>
+                        <GiHamburgerMenu
+                            onClick={menuBurger}
+                            className="burger-menu"
+                        />
+                    </div>
+                </div>
+
+                <ul className={`menu-nav-links ${menuOpen ? "open" : ""}`}>
+                {/* <ul className={`menu-nav-links open`}> */}
                     <li>
-                        <a href="#home">Home</a>
+                        <a href="#home" onClick={menuBurger}>
+                            Home
+                        </a>
                     </li>
                     <li>
-                        <a href="#about">About</a>
+                        <a href="#about" onClick={menuBurger}>
+                            About
+                        </a>
                     </li>
                     <li>
-                        <a href="#projects">Projects</a>
+                        <a href="#projects" onClick={menuBurger}>
+                            Projects
+                        </a>
                     </li>
                     <li>
-                        <a href="#contact">Contact</a>
+                        <a href="#contact" onClick={menuBurger}>
+                            Contact
+                        </a>
                     </li>
                 </ul>
-            </div>
-        </nav>
+            </nav>
+        </>
     );
 }
 
