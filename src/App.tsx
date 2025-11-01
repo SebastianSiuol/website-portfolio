8ec82b97125aa339467b947d24cb254ce3d4374b
SebastianSiuol <sebastianlouismtorio@iskolarngbayan.pup.edu.ph>
2025-11-01 11:02:36 +0800

    feat: add tsparticles for enhanced particle effects; update styles for backdrop blur in TechStackCard and Projects sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { useCallback, useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import type { Container, Engine } from "@tsparticles/engine";
+// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+
+
 // Imports
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
@@ -12,9 +21,94 @@ import Projects from "./sections/Projects";
 import "./App.css";
 
 function App() {
+    const [ init, setInit ] = useState(false);
+
+    // this should be run only once per application lifetime
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+            // starting from v2 you can add only the features you need reducing the bundle size
+            //await loadAll(engine);
+            //await loadFull(engine);
+            await loadSlim(engine);
+            //await loadBasic(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
 
     return (
         <>
+        { init && <Particles
+            id="tsparticles"
+            particlesLoaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "#111",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                    },
+                    modes: {
+                        push: {
+                            quantity: 1,
+                        },
+                        repulse: {
+                            distance: 100,
+                            duration: 0.4,
+                        },
+                    },
+                    resize: true,
+                },
+                particles: {
+                    color: {
+                        value: "#fff",
+                    },
+                    links: {
+                        color: "#666",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: true,
+                        speed: 2,
+                        straight: false,
+                    },
+                    number: {
+                        value: 10,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 2, max: 3 },
+                    },
+                },
+            }}
+        />
+}
             <NavBar />
             <Home />
             <About />
